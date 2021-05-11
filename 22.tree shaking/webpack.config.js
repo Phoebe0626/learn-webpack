f6770a72e1f550a63cb264b27f9e58936b354085
phoebe <506014196@qq.com>
2021-05-11 20:03:46 +0800

    tree-shaking

--- 22.tree shaking/webpack.config.js
+++ 22.tree shaking/webpack.config.js
@@ -4,16 +4,19 @@ const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plug
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /**
- * tree shaking：
- *  目的：去除无用代码
- *  前提： 1. 必须使用 ES6 模块化 2. 开启 production 模式
- *  作用：减少代码体积
- * 
- * 在 package.json 中配置
- *  "sideEffects": false 所有代码都没有副作用（都可以进行 tree shaking）
- *    问题：可能会把 css / @babel/polyfill (副作用) 文件干掉
- *    解决：
- *      "sideEffects": ["*.css", "*.less"]
+ * 缓存：
+ *  babel 缓存
+ *    cacheDirectory: true
+ *    --> 让第二次打包构建速度更快
+ *  文件资源缓存
+ *    hash: 每次 webpack 打包会生成一个唯一的 hash 值
+ *    问题: 因为 js 和 css 同时使用一个 hash 值，
+ *         如果重新打包，会导致所有缓存失效。（可能我只改动了一个文件）
+ *    chunkhash: 根据 chunk 生成的 hash 值。如果打包来源于同一个 chunk，那么 hash 值就一样
+ *      问题: js 和 css 的 hash 值还是一样的
+ *           因为 css 是在 js 中被引入的，所以同属于一个 chunk
+ *    contenthash: 根据文件的内容生成 hash 值。不同文件 hash 值一定不一样
+ *    --> 让代码上线运行缓存更好使用
  */
 
 // 定义 nodejs 环境变量：决定使用 browserslist 的哪个环境
